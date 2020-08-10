9dee98e854188c1638ba81dbf30ea59b7d40bbfd
lukaBRI <beccibrian@gmail.com>
2020-08-10 13:47:37 -0300

    Heroku run99

--- src/bot.js
+++ src/bot.js
@@ -21,18 +21,18 @@ client.connect();
 
 //Hacemos que el bot envie un mensaje al estar conectado al canal
 
-client.on('connected', (addres, port) =>{
+client.on('connected', async(addres, port) =>{
     //creamos una accion para el canal
-    client.action('lucadevbot', `hello ${addres}: ${port}`)
+    await client.action('lucadevbot', `hello ${addres}: ${port}`)
 })
 
-client.on('chat', (target, ctx, message, self) =>{
+client.on('chat', async(target, ctx, message, self) =>{
     if(self) return;
     //Sacamos los espacios del comando para despues validarlo
     const commandName = message.trim()
 
     //Validamos el mensaje segun la accion que queremos
     if( commandName === '!say'){
-        client.say(target, `Welcome ${ctx.username}`)
+        await client.say(target, `Welcome ${ctx.username}`)
     }
 })
